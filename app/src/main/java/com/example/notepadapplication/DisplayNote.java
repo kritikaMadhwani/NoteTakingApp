5b15d24dd48e6e49105a08e2ab5b1b3c7314ac6d
Kritika.m <kritika.m@tataelxsi.co.in>
2020-02-25 17:45:00 +0530

    Missing String Resource

--- app/src/main/java/com/example/notepadapplication/DisplayNote.java
+++ app/src/main/java/com/example/notepadapplication/DisplayNote.java
@@ -1,35 +1,35 @@
 package com.example.notepadapplication;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.arch.persistence.room.Room;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.example.kritika_m.notetaking.R;
+import com.example.kritika_m.notetaking.notedb.NoteDatabase;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 public class DisplayNote extends AppCompatActivity {
 
     EditText name;
     EditText content;
     private CoordinatorLayout coordinatorLayout;
-    String dateString;
-    Bundle extras;
     int id_To_Update = 0;
     Snackbar snackbar;
+    NoteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        db = Room.databaseBuilder(getApplicationContext(),
+                NoteDatabase.class, "database-name").build();
+
+
         setContentView(R.layout.create_notes);
         name = (EditText) findViewById(R.id.txtname);
         content = (EditText) findViewById(R.id.txtcontent);
@@ -39,22 +39,13 @@ public class DisplayNote extends AppCompatActivity {
         if (extras != null) {
             int Value = extras.getInt("id");
             if (Value > 0) {
-                snackbar = Snackbar
-                        .make(coordinatorLayout, "Note Id : "+String.valueOf(Value), Snackbar.LENGTH_LONG);
+                snackbar = Snackbar.make(coordinatorLayout, "Note Id : " + String.valueOf(Value), Snackbar.LENGTH_LONG);
                 snackbar.show();
-               // Cursor rs = mydb.getData(Value);
                 id_To_Update = Value;
-              //  rs.moveToFirst();
-               // String nam = rs.getString(rs.getColumnIndex(NDb.name));
-               // String contents = rs.getString(rs.getColumnIndex(NDb.remark));
-             //   if (!rs.isClosed()) {
-               //     rs.close();
-              //  }
-              //  name.setText((CharSequence) nam);
-               // content.setText((CharSequence) contents);
             }
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         Bundle extras = getIntent().getExtras();
@@ -64,85 +55,7 @@ public class DisplayNote extends AppCompatActivity {
         }
         return true;
     }
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        switch (item.getItemId()) {
-            case R.id.Delete:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage(R.string.DeleteNote)
-                        .setPositiveButton("YES",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,
-                                                        int id) {
-                                       // mydb.deleteNotes(id_To_Update);
-                                        Toast.makeText(DisplayNote.this, "Deleted Successfully",Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(
-                                                getApplicationContext(), MainActivity.class);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-                                })
-                        .setNegativeButton("NO",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,
-                                                        int id) {
-                                    }
-                                });
-                AlertDialog d = builder.create();
-                d.setTitle("Are you sure");
-                d.show();
-                return true;
-            case R.id.Save:
-                Bundle extras = getIntent().getExtras();
-                Calendar c = Calendar.getInstance();
-                System.out.println("Current time => " + c.getTime());
-                SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-                String formattedDate = df.format(c.getTime());
-                dateString = formattedDate;
-                if (extras != null) {
-                    int Value = extras.getInt("id");
-                    if (Value > 0) {
-                        if (content.getText().toString().trim().equals("")
-                                || name.getText().toString().trim().equals("")) {
-                            snackbar = Snackbar
-                                    .make(coordinatorLayout, "Please fill in name of the note", Snackbar.LENGTH_LONG);
-                            snackbar.show();
-                        } else {
-                           /* if (mydb.updateNotes(id_To_Update, name.getText().toString(), dateString, content.getText().toString())) {
-                                snackbar = Snackbar
-                                        .make(coordinatorLayout, "Your note Updated Successfully!!!", Snackbar.LENGTH_LONG);
-                                snackbar.show();
-                            } else {
-                                snackbar = Snackbar
-                                        .make(coordinatorLayout, "There's an error. That's all I can tell. Sorry!", Snackbar.LENGTH_LONG);
-                                snackbar.show();
-                            }*/
-                        }
-                    } else {
-                        if (content.getText().toString().trim().equals("")
-                                || name.getText().toString().trim().equals("")) {
-                            snackbar = Snackbar
-                                    .make(coordinatorLayout, "Please fill in name of the note", Snackbar.LENGTH_LONG);
-                            snackbar.show();
-                        } else {
-                            /*if (mydb.insertNotes(name.getText().toString(), dateString,
-                                    content.getText().toString())) {
-                                snackbar = Snackbar
-                                        .make(coordinatorLayout, "Added Successfully.", Snackbar.LENGTH_LONG);
-                                snackbar.show();
-                            } else {
-                                snackbar = Snackbar
-                                        .make(coordinatorLayout, "Unfortunately Task Failed.", Snackbar.LENGTH_LONG);
-                                snackbar.show();
-                            }*/
-                        }
-                    }
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(
