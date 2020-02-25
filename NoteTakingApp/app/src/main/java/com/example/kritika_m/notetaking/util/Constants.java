5653ab1c7e35cb5e29325c04fa9e0acec85693a6
Kritika.m <kritika.m@tataelxsi.co.in>
2020-02-25 16:38:03 +0530

    Intializing the repository

---
+++ NoteTakingApp/app/src/main/java/com/example/kritika_m/notetaking/util/Constants.java
@@ -0,0 +1,13 @@
+package com.example.kritika_m.notetaking.util;
+
+/**
+ * Created by kritika_m on 02/22/2020.
+ */
+
+final public class Constants {
+    private Constants() {
+    }
+
+    public static final String TABLE_NAME_NOTE = "notes";
+    public static final String DB_NAME = "notesdb.db";
+}
